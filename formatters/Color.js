ca3f49113f4b5704aa070cb21d42a3c79de56419
Moritz Roessler <moritz.roessler@gmail.com>
2020-09-07 13:30:27 +0200

    map Color.isString to an array

--- formatters/Color.js
+++ formatters/Color.js
@@ -8,8 +8,8 @@ chalk.level = 3;
 class Color extends MapFormatter {
   static colorByType = type => v => !this.colors.type[type]?v:chalk.keyword(this.colors.type[type])(v);
   static formatMap = new Map([
-    [oftype('string'), Color.compose(/*util.inspect,*/v => !this.colors.type.string?v:chalk.keyword(this.colors.type.string)(v))],
-    [Color.isString, [chalk.keyword('cyan')]],
+    [Color.isString, [v => !this.colors.type.string?v:chalk.keyword(this.colors.type.string)(v))]],
+    [oftype('number'), chalk.keyword('cyan')],
     [ofinstance(Error), v => chalk.keyword('red')(v.message)],
     [Color.isObject, [Color.colorByType('object')]],
     [/warning/i, (m, r) => m.replace(r, (val) => {
