78f57ecf8293490b96fd6a3cb79aab3f7e1fe531
Moritz Roessler <moritz.roessler@gmail.com>
2020-09-11 19:04:22 +0200

    added jsdoc to consoletransport

--- transports/ChromeTransport.js
+++ transports/ChromeTransport.js
@@ -1,45 +1,90 @@
-var AU = require('ansi_up');
-var ansi_up = new AU.default;
+const AU = require('ansi_up').default;
+const ansiUp = new AU;
 
 const {ConsoleTransport} = require('./ConsoleTransport');
 
-function styledConsoleLog() {
-  var argArray = [];
+/**
+ * @typedef any
+ * @type {string|number|array|object} - Any type
+ */
+
+/**
+ * @param {string} message - A HTML styled message.
+ * @param {...any} args - Optional arguments passed to console.log.
+ * @return {any[]} - An array of arguments for console.log, styled using css format strings.
+ */
+function getStyledConsoleLogArgs(message = '', ...args) {
+  const argArray = [];
 
   if (arguments.length) {
-      var startTagRe = /<span\s+style=(['"])([^'"]*)\1\s*>/gi;
-      var endTagRe = /<\/span>/gi;
+    const startTagRe = /<span\s+style=(['"])([^'"]*)\1\s*>/gi;
+    const endTagRe = /<\/span>/gi;
 
-      var reResultArray;
-      argArray.push(arguments[0].replace(startTagRe, '%c').replace(endTagRe, '%c'));
-      while (reResultArray = startTagRe.exec(arguments[0])) {
-          argArray.push(reResultArray[2]);
-          argArray.push('');
-      }
+    let reResultArray;
+    argArray.push(message.replace(startTagRe, '%c').replace(endTagRe, '%c'));
+    while (reResultArray = startTagRe.exec(message)) {
+      argArray.push(reResultArray[2]);
+      argArray.push('');
+    }
 
-      // pass through subsequent args since chrome dev tools does not (yet) support console.log styling of the following form: console.log('%cBlue!', 'color: blue;', '%cRed!', 'color: red;');
-      for (var j = 1; j < arguments.length; j++) {
-          argArray.push(arguments[j]);
-      }
+    // pass through subsequent args since chrome dev tools does
+    // not (yet) support console.log styling of the following 
+    // form: 
+    // console.log('%cBlue!', 'color: blue;', '%cRed!', 'color: red;');
+    for (const arg of args) {
+      argArray.push(arg);
+    }
   }
 
   return argArray;
-  console.log.apply(console, argArray);
 }
 
+
+/**
+ * The return value of ConsoleTransport.prototype.transform
+ * @typedef {object.<object, any>} ConsoleTransportTransformedOptions
+ * @property {object} console - Additional options used by ConsoleTransports
+ * @property {any[]} console.args - Arguments intended to be passed to console.log
+ * @property {any} * - any
+ */
+
+ /**
+ * @typedef Person
+ * @type {Object}
+ * @property {number} age - the person's age
+ * @property {string} hobby - the person's hobby
+ */
+
+/**
+ * @typedef {Object.<string, Person>} Descriptor
+ */
+
+/** Chrome transport class. */
 class ChromeTransport extends ConsoleTransport {
-  transform (options) {
-    const styled = ansi_up.ansi_to_html(options.message)
-    const args = styledConsoleLog(styled);
+  /**
+   * Transforms the options passed to the function.
+   * Adds options.console.args
+   * @param {object} options - An arbitrary object.
+   * @return {ConsoleTransportTransformedOptions} - The transformed options.
+   */
+  transform(options) {
+    options = super.transform(options);
+    const styled = ansiUp.ansi_to_html(options.message);
+    const args = getStyledConsoleLogArgs(styled);
+    // console.log(styled);
     options.console = options.console || {};
     options.console.args = args;
     return options;
   }
-  log (options) {
-    console.log.apply(console, options.console.args);
+
+  /**
+   * @param {object} options - An arbitrary object.
+   */
+  log(options) {
+    console.log.call(console, ...options.console.args);
   }
 }
 
 module.exports = {
-  ChromeTransport
-}
+  ChromeTransport,
+};
