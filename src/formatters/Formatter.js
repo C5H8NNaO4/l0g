c11873111a1ce8beabe51609ae07d5d6405e6ccb
Moritz Roessler <moritz.roessler@gmail.com>
2021-03-02 16:38:57 +0100

    fix: remove unneccesary import

--- src/formatters/Formatter.js
+++ src/formatters/Formatter.js
@@ -1,17 +1,16 @@
 const {tag} = require('../tags/tag');
-const moment = require('moment')
-const util = require('util');
-const { Format } = require('../symbols');
-const { Logger } = require('../Logger');
+const moment = require('moment');
+const {Format} = require('../symbols');
 
 class Formatter {
-  constructor (fn) {
-    if (typeof fn === 'function')
+  constructor(fn) {
+    if (typeof fn === 'function') {
       this._format = fn;
-    this.foo = "bar"
+    }
+    this.foo = 'bar';
   }
 
-  transformTagArgs (options, strs, ...args) {
+  transformTagArgs(options, strs, ...args) {
     const transformed = args.map((obj) => {
       if (obj && obj[Format]) return obj[Format]({formatter: this, ...options});
       return obj;
@@ -19,31 +18,32 @@ class Formatter {
     return [strs, ...transformed];
   }
 
-  transform (options) {
+  transform(options) {
     if (Array.isArray(options.tag) && options.tag[0].raw) {
       options.tag = this.transformTagArgs(options, ...options.tag);
       options.message = this.tag(...options.tag);
     }
-    
+
     options.ts = this.constructor.timestamp(options.ts);
 
     return options;
   }
 
-  format (options) {
+  format(options) {
     const {ts, level, message} = options;
-    if (this._format)
+    if (this._format) {
       return this._format(options);
+    }
     return `${ts} ${level}: ${message}`;
   }
 
-  static timestamp (ts, options) {
+  static timestamp(ts, options) {
     return moment(ts).toISOString();
   }
 
   // static transform (instance, options) {
   //   // if (typeof instance.transformTag === 'function')
-  //   //   options.tag = 
+  //   //   options.tag =
   //   if (Array.isArray(options.tag) && options.tag[0].raw)
   //     options.message = instance.tag(...options.tag);
 
@@ -54,8 +54,8 @@ class Formatter {
   //   return options;
   // }
 
-  static format (instance, options) {
-    options = instance.transform(options) //Formatter.transform(instance, options);
+  static format(instance, options) {
+    options = instance.transform(options); // Formatter.transform(instance, options);
     return instance.format(options);
   }
 }
